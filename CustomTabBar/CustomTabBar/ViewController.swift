02ade8b48ec24d5647ff60c7cab4308cfeb68cf8
EE201201 <ios.devpino@platb.ai>
2021-03-31 14:28:41 +0900

    Create CustomTabbar Open Souce  ~ing

---
+++ CustomTabBar/CustomTabBar/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  CustomTabBar
+//
+//  Created by EE201201 on 2021/03/31.
+//
+
+import UIKit
+
+class ViewController: JBTabBarController {
+
+    let home = HomeViewController()
+    let setting = SettingViewController()
+
+    override func loadView() {
+        super.loadView()
+
+        // 탭바 입맛에 맞게 지정
+        /*
+
+
+
+         */
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+
+}
+
